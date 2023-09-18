a9005f348f4132f5c261577a39df36bfa63cd839
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-09-18 11:05:44 -0300

    PR-01: added husky and babel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`app listening in port ${port}`);
