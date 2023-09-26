9bbec5576554fe6b538328be0b4f8681db250e37
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-09-26 11:13:52 -0300

    PR-00: added some routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import app from './app';
 
-const app = express();
+dotenv.config();
 
-const port = 3000;
+mongoose
+  .connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('MongoDB connected');
 
-app.listen(port, () => {
-  console.log(`app listening in port ${port}`);
+    try {
+      app.listen(process.env.PORT, (err) => {
+        if (err) throw err;
+        // eslint-disable-next-line no-console
+        console.log(`server listening on port: ${process.env.PORT}`);
+      });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log('There was an error starting the server: ', err);
+    }
+  })
+  // eslint-disable-next-line no-console
+  .catch((e) => console.log(e));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
