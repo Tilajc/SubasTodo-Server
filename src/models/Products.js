9758f7cac7afbf03cecb57137406313f8a1a5343
Victoria Negrotto <vnegrotto19@gmail.com>
2023-10-06 12:22:39 -0300

    feature/ST-29/products: start work on Products

---
+++ src/models/Products.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema ({
+
+name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+export default mongoose.model('Products', productSchema);
+
