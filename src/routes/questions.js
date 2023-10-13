5e2d4923d72c35cbf6e83702a5d3939d22fa2f4e
Victoria Negrotto <vnegrotto19@gmail.com>
2023-10-13 13:18:49 -0300

    ST-29: advanced on routes and tested in postman

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,7 +1,15 @@
-import { Router } from 'express';
-const router = Router();
+import express from 'express';
+const router = express.Router();
+import validations from '../validations/questions';
 import { default as QuestionsController } from '../controllers/questions';
 
-router.put('/questions/:id', QuestionsController.updateQuestion);
+
+
+router
+  .get('/', QuestionsController.getAllQuestions)
+  .get('/:id', QuestionsController.getQuestionById)
+  .post('/create', QuestionsController.createQuestion, validations.questionValidateCreation)
+  .put('/update/:id', QuestionsController.updateQuestion, /*validations.ValidateUpdate*/)
+  .delete('/delete/:id', QuestionsController.deleteQuestion);
 
 export default router;
