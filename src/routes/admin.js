bc897d371cb9103caed389c984640decdd8514c8
unknown <lstradiot15@gmail.com>
2023-10-29 17:48:37 -0300

    Added validations and this to the routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,4 +1,5 @@
 import adminControllers from '../controllers/admin';
+import validations from '../validations/admin';
 import express from 'express';
 
 const router = express.Router();
@@ -6,8 +7,8 @@ const router = express.Router();
 router
   .get('/', adminControllers.getAdmins)
   .get('/:id', adminControllers.getAdminsById)
-  .post('/', adminControllers.createAdmin)
-  .put('/:id', adminControllers.updateAdmin)
+  .post('/', validations.adminValidateCreation, adminControllers.createAdmin)
+  .put('/:id', validations.adminValidateUpdate, adminControllers.updateAdmin)
   .delete('/:id', adminControllers.deleteAdmin);
 
 export default router;
